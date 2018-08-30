957be99ea1ed9e2730a63949d573508f205f0f1d
Unknown <compass2k17@outlook.com>
2018-08-30 14:38:53 -0500

    Implement offline service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,19 +2,20 @@
 
 const CACHE_VERSION = 'v1';
 
-self.addEventListener('activate', e => {
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
 
-  console.log("running activate")
+self.addEventListener('activate', e => {
 
   e.waitUntil(
     caches.keys()
       .then(names => Promise.all(
             names
-            .filter(name => name !== CACHE_VERSION)
-            .map(cache => caches.delete(cache))
-        ))
+              .filter(name => name !== CACHE_VERSION)
+              .map(cache => caches.delete(cache))
+      ))
       .catch(err => {
-        console.log("oops??")
         throw err;
       })
   );
@@ -22,11 +23,9 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.repondWith(
+  e.respondWith(
     caches.open(CACHE_VERSION)
-      .then(cache => {
-        console.log(`found ${  cache}`)
-        return cache.match(e.request)
+      .then(cache => cache.match(e.request)
           .then(match => {
 
             if (match) {
@@ -41,8 +40,7 @@ self.addEventListener('fetch', e => {
           })
           .catch(err => {
             throw err;
-          });
-      })
+          }))
       .catch(err => {
         throw err;
       })
